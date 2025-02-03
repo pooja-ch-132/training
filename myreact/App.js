61a512617bc6bbec8249799f8102810b72481350
pooja-ch-132 <158035573+pooja-ch-132@users.noreply.github.com>
2025-02-03 13:48:39 +0530

    Add files via upload

---
+++ myreact/App.js
@@ -0,0 +1,42 @@
+// import React from "react";
+// function App() {
+//   // let ele = React.createElement(
+//   //   "div",
+//   //   { className: "App" },
+//   //   React.createElement("h1", null, "Header")
+//   // );
+//   let username = "Dhoni";
+//   return (
+//     <div className="App">
+//       <h1 style={{ color: "red" }}>ReactJS Session-1</h1>
+//       <input />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//!components
+
+//!CBC
+import React from "react";
+class App extends React.Component {
+  render() {
+    return <h1>Class based components</h1>;
+  }
+}
+export default App;
+
+//!FBC
+// import React from "react";
+// function App() {
+//   return <h1>Function based components</h1>;
+// }
+// export default App;
+
+//!OR
+// import React from "react";
+// const App = () => {
+//   return <h1>Arrow function components</h1>;
+// };
+// export default App;
